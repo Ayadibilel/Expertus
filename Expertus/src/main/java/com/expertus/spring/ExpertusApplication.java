8d58954419cc4582437016546f23c2dde7dbc222
Bilel <Bilel@DESKTOP-G35CNH0>
2019-09-05 23:52:19 +0100

    First commit

---
+++ Expertus/src/main/java/com/expertus/spring/ExpertusApplication.java
@@ -0,0 +1,13 @@
+package com.expertus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertusApplication.class, args);
+	}
+
+}
