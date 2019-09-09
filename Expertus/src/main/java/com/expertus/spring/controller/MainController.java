a722ad7ebd0842ffc8d167578b939d7cd13fb583
Bilel <Bilel@DESKTOP-G35CNH0>
2019-09-10 00:35:04 +0100

    10/09/2019

--- Expertus/src/main/java/com/expertus/spring/controller/MainController.java
+++ Expertus/src/main/java/com/expertus/spring/controller/MainController.java
@@ -11,4 +11,36 @@ public class MainController {
 	public String Home() {
 		return"index";
 	}
+	@RequestMapping(value="/about")
+	public String about() {
+		return"about";
+	}
+	@RequestMapping(value="/action")
+	public String action() {
+		return"call-to-actions";
+	}
+	@RequestMapping(value="/checkout")
+	public String chekout() {
+		return"checkout";
+	}
+	@RequestMapping(value="/contact")
+	public String contact() {
+		return"contact-us";
+	}
+	@RequestMapping(value="/faq")
+	public String faq() {
+		return"faq";
+	}
+	@RequestMapping(value="/service")
+	public String service() {
+		return"service-single";
+	}
+	@RequestMapping(value="/feature")
+	public String feature() {
+		return"template-feature";
+	}
+	@RequestMapping(value="/test")
+	public String test() {
+		return"testimonials";
+	}
 }
